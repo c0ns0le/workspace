b54c3f8bd686bf5e58d330ceb474861bb66d4fbb
@dkfiresky <dkfiresky@users.noreply.github.com>
2016-02-27 22:39:00 +0200

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -4,7 +4,7 @@ if ( $url -eq '' ) {
 	exit -2;
 }
 
-$storageDir = C:\Workspace\Downloads;
+$storageDir = "C:\Workspace\Downloads";
 
 $webClient = New-Object System.Net.WebClient;
 $webClient.DownloadFile( $url, $storageDir );
